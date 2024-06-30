679213ede8d319593e50ce74b19183a25bf0bcf5
Igli <iglikoxha@outlook.com>
2024-07-01 01:29:25 +0200

    LazyVim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,8 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	config = function()
-		require("gruvbox").setup({
-			transparent_mode = true,
-		})
-		vim.cmd.colorscheme("gruvbox")
-	end,
+  "ellisonleao/gruvbox.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = {
+    transparent_mode = true,
+  },
 }
