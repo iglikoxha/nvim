47a1ddc37b8168bf6d2b98b11b66ece4b2b7b101
Igli <iglikoxha@outlook.com>
2024-07-02 23:37:57 +0200

    Initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent_mode = true,
+    },
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+}
