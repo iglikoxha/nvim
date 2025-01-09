fbce3a3ce12d6ddb520a4a286372ba68bbea25fd
Igli <iglikoxha@outlook.com>
2025-01-09 16:39:53 +0100

    New config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
--- Hide hightlight on search
 vim.opt.hlsearch = false
 vim.opt.guicursor = ""
 vim.opt.colorcolumn = "80"
+vim.opt.swapfile = false
